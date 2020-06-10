f39f8a4f83b11e821c4cad1dec280e4a232188eb
Andres Kim <andreskimlee@gmail.com>
2020-06-09 20:26:13 -0400

    Add routes

---
+++ AngularNetCoreProject/ClientApp/src/routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router'
+import { SectionSalesComponent } from './section/section-sales/section-sales.component'
+import { SectionOrdersComponent } from './section/section-orders/section-orders.component'
+import { SectionHealthComponent } from './section/section-health/section-health.component'
+
+export const appRoutes: Routes = [
+    { path: "sales", component: SectionSalesComponent },
+    { path: "orders", component: SectionOrdersComponent },
+    { path: "sales", component: SectionHealthComponent },
+    { path: '', redirectTo: './sales', pathMatch: 'full' }
+    // pathMatch is pretty much equivalent to exact path. 
+]
